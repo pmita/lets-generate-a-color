40623c9d714af688378104dfb4529cf1a75f501b
pmita <panos.mita2@outlook.com>
2021-04-18 22:02:14 +0100

    re-factoring

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-/*Project objectives: */
+/*
+    Fun projects that goes over basic principles in javascript. Here are the objectives:
+    #Generate a random hex color whenever a user presses the button
+    #Change the background of our website accordingly
+    #Change the header text to the corresponding color
+
+    Remember to always apply clean code principles in every project you are making! Here are
+    some basic rules to have at the back of your mind:
+
+    #Leave commects whenever your code is doing something complicated
+    #Try giving your variables usefull names. Best if their name describes what they correspond to
+    #Re-factor your code once you get something done. Chances are you can make better
+*/
 
 
+/*------------Global Variables-------------*/
+const arrayOfHexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const generateColor_Button = document.querySelector('.btn');
+const hexColor_header = document.querySelector('.color');
 
-const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
-
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
-
-btn.addEventListener('click', function(){
-    //get random number [0,3]
-    const randomNumber = getRandomNumbers();
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-
-    color.textContent = colors[randomNumber];
+/*------------Event Listners---------------*/
+generateColor_Button.addEventListener('click', function(){
+    let randomHexColor = '#';
+    /*To generate a hex simply select a random character from the arrayOfHexCharacters, 6 times*/
+    for(let i=0; i<6; i++){
+        randomHexColor += arrayOfHexCharacters[generateHexCharacter()];
+    }
+    
+    //Apply the nex color to both the background and the header
+    hexColor_header.textContent = randomHexColor;
+    document.body.style.backgroundColor = randomHexColor;
 });
 
-function getRandomNumbers(){
-
+/*-----------Functions-------------------*/
+function generateHexCharacter(){
     //Math.random() gives numbers [0, 0.9999999)
     //Math.floor() rounds the number to the closest number
     //Math.floor(Math.random()) will spit a random number, rounded
-    return Math.floor(Math.random() * colors.length); 
+    return Math.floor(Math.random() * arrayOfHexCharacters.length);
 }
